3bb371a28ac83d212a233ba2760733f9484baa44
Barry Kostjens <b.kostjens@prosilic.nl>
2019-04-19 13:51:28 +0200

    Packages update

--- Package.swift
+++ Package.swift
@@ -2,16 +2,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "Turnstile",
-	products: [
-    .library(name: "Turnstile", targets: ["Turnstile"]),
-	],
+    name: "ts",
+ products: [
+        .library(name: "Turnstile", targets: ["Turnstile"]),
+    ],
     targets: [
         .target(
             name: "Turnstile",
             dependencies: ["TurnstileCrypto"]),
         .target(
-            name: "TurnstileCrypto"),
+            name: "TurnstileCrypto",
+	   dependencies: []),
         .target(
             name: "TurnstileWeb",
             dependencies: ["Turnstile"]),
